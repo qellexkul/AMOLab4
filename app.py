2b631f94579f164790eb06d5efe8c2e2f770ab44
Alexander Shmachenko <fmeemf@mail.ru>
2025-04-11 14:42:53 +0700

    new dataset

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import pandas as pd
 # Загрузка данных
 train_df, _ = titanic()
 
-# Сохранение в CSV
-# train_df.to_csv("titanic.csv", index=False)
-df = train_df[["Pclass", 'Sex', 'Age']]
-df.to_csv("titanic.csv", index=False)
+# Чтение данных
+df = pd.read_csv("date/titanic.csv")
+
+# Выбор нужных признаков
+selected_features = df[['Pclass', 'Sex', 'Age']]
+
+# Сохранение нового датасета
+selected_features.to_csv("date/selected_features.csv", index=False)
